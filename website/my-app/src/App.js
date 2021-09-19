0b57f99604b5fce2ef30b4e3934764b1e1338d50
alysekwok <ALYSEKWOKK@GMAIL.COM>
2021-09-19 03:51:42 -0400

    updated home page

--- website/my-app/src/App.js
+++ website/my-app/src/App.js
@@ -1,17 +1,20 @@
-
+import textbookImage from './images/textbookclipart.png';
 import './App.css';
+import About from './About.js';
+import HomeImage from './HomeImage.js';
 
 
 
 function App() {
   return (
     <div class = "App">
-    <h3>welcome to </h3>
-    <h1 id = "header ">book exchange</h1>
-    <h2><i>a community of resources, at 
-    your fingertips </i></h2>
-    <input type = "text" placeholder = "Search for a book here"></input>
-    <button>Search</button>
+      <div id = "logo">
+    <h1 id = "header">book exchange</h1>
+    <img id = "clipart" src = {textbookImage} />
+    </div>
+    
+    <HomeImage/>
+    <About/> 
     </div>
   );
 }
