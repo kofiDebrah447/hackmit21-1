156b8b09317fb1c287359eb8df1831e71f4a3155
alysekwok <ALYSEKWOKK@GMAIL.COM>
2021-09-19 01:58:30 -0400

    new draft

---
+++ website/my-app/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div class = "App">
+    <h3>welcome to </h3>
+    <h1 id = "header ">book exchange</h1>
+    <h2><i>a community of resources, at 
+    your fingertips </i></h2>
+    <input type = "text" placeholder = "Search for a book here"></input>
+    <button>Search</button>
+    </div>
+  );
+}
+
+export default App;
